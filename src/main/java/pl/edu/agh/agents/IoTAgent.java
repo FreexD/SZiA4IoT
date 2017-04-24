d619164268ced22b40d5d894a5ad8086f31ff184
Michał Wolny <michalwoln@gmail.com>
2017-04-24 19:13:23 +0200

    create project, add jade deps, create IoT agent

---
+++ src/main/java/pl/edu/agh/agents/IoTAgent.java
@@ -0,0 +1,86 @@
+package pl.edu.agh.agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+/**
+ * Created by mw on 24/04/17.
+ *
+ * Class implementing IoT device behavior.
+ *
+ * @author mw
+ * @see jade.core.Agent
+ */
+public class IoTAgent extends Agent {
+
+    private int temperature;
+    private static final int BOTTOM_TEMPERATURE_LIMIT = -20;
+    private static final int TOP_TEMPERATURE_LIMIT = 30;
+
+    /**
+     * Run on agent initialization.
+     */
+    protected void setup(){
+        System.out.println("IoT agent " + getAID().getName() + " initialized.");
+        addBehaviour(new IoTReceivingBehavior());
+    }
+
+    /**
+     * Run on agent termination. Do cleanup stuff here.
+     */
+    protected void takeDown(){
+        System.out.println("IoT agent " + getAID().getName() + " terminating.");
+    }
+
+    private int getTemperature(){
+        return temperature;
+    }
+
+    private void setTemperature(int temperature){
+        this.temperature = temperature;
+    }
+
+    /**
+     * Cyclic receiving behavior for IoT device.
+     *
+     * @see jade.core.behaviours.CyclicBehaviour
+     * @see jade.lang.acl.MessageTemplate
+     * @see jade.lang.acl.ACLMessage
+     */
+    private class IoTReceivingBehavior extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+            ACLMessage msg = myAgent.receive(mt);
+            if(msg != null){
+                String message = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                if(message.equals("get")){
+                    reply.setPerformative(ACLMessage.INFORM);
+                    reply.setContent(String.valueOf(getTemperature()));
+                } else {
+                    try {
+                        Integer suggestedTemperature = Integer.parseInt(message);
+                        if(suggestedTemperature >= BOTTOM_TEMPERATURE_LIMIT && suggestedTemperature <= TOP_TEMPERATURE_LIMIT){
+                            reply.setPerformative(ACLMessage.INFORM);
+                            setTemperature(suggestedTemperature);
+                            reply.setContent(String.valueOf(getTemperature()));
+                        } else {
+                            reply.setPerformative(ACLMessage.FAILURE);
+                            reply.setContent("Temperature is not between " + BOTTOM_TEMPERATURE_LIMIT + " and " + TOP_TEMPERATURE_LIMIT + ".");
+                        }
+                    } catch (NumberFormatException e) {
+                        reply.setPerformative(ACLMessage.FAILURE);
+                        reply.setContent("Temperature is not a valid integer.");
+                    }
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+
+        }
+    }
+}
