81463b260d019c24ab118035cfc01b78ab4409a7
szwojtkowski <wojtkowski.sz@gmail.com>
2017-04-24 23:23:39 +0200

    added searching for temperature sensors behaviour

--- src/main/java/pl/edu/agh/agents/controller/ControllerAgent.java
+++ src/main/java/pl/edu/agh/agents/controller/ControllerAgent.java
@@ -1,7 +1,13 @@
 package pl.edu.agh.agents.controller;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +17,15 @@ public class ControllerAgent extends Agent {
 
     private ControllerGui gui;
     private int preferredTemperature;
+    private AID[] temperatureSensors;
+    private int interval = 20000;
 
     protected void setup() {
         logger.info("Controller agent " + getAID().getLocalName() + " created.");
         preferredTemperature = 22;
         gui = new ControllerGui(this);
         gui.display();
+        addBehaviour(new SearchSensorsBehaviour(this, interval));
     }
 
     protected void takeDown() {
@@ -31,6 +40,31 @@ public class ControllerAgent extends Agent {
                 logger.info("Set preferred temperature to: " + preferredTemperature);
             }
         });
+    }
+
+    private class SearchSensorsBehaviour extends TickerBehaviour {
+
+        public SearchSensorsBehaviour(Agent a, long period) {
+            super(a, period);
+        }
+
+        @Override
+        protected void onTick() {
+            logger.info("Getting temperature from sensors.");
+            DFAgentDescription template = new DFAgentDescription();
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType("temperature-sensor");
+            template.addServices(sd);
+            try {
+                logger.info("Searching for temperature sensors.");
+                DFAgentDescription[] result = DFService.search(myAgent, template);
+                temperatureSensors = new AID[result.length];
+                if(result.length == 0)
+                    logger.info("Not found any temperature sensor.");
+            } catch (FIPAException fe) {
+                fe.printStackTrace();
+            }
+        }
     }
 
 }
