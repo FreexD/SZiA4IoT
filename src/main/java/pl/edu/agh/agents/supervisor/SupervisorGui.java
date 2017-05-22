6f4bb60c449a0cfc90881c3d02e547b437bdde97
szwojtkowski <wojtkowski.sz@gmail.com>
2017-05-23 00:55:13 +0200

    created supervisor gui elements

--- src/main/java/pl/edu/agh/agents/supervisor/SupervisorGui.java
+++ src/main/java/pl/edu/agh/agents/supervisor/SupervisorGui.java
@@ -4,17 +4,23 @@ import jade.core.AID;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class SupervisorGui extends JFrame {
 
     private SupervisorAgent agent;
-    private JComboBox box;
+    private JComboBox<String> box;
+    private JTextField maxTemperatureField;
+    private JTextField minTemperatureField;
+    private JLabel currentTemperatureLabel;
 
     SupervisorGui(SupervisorAgent agent){
         this.agent = agent;
         createControllersBox();
+        createControllerOptions();
+        createSaveButton();
         addWindowListener(new	WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 agent.doDelete();
@@ -33,12 +39,59 @@ public class SupervisorGui extends JFrame {
     }
 
     private void createControllersBox() {
-        box = new JComboBox();
-        JLabel label1 = new JLabel("Controllers");
+        box = new JComboBox<String>();
+        JLabel label1 = new JLabel("Controllers ");
         JPanel panel1 = new JPanel(new BorderLayout());
         panel1.add(label1, BorderLayout.WEST);
         panel1.add(box, BorderLayout.CENTER);
         add(panel1, BorderLayout.NORTH);
+    }
+
+    private void createControllerOptions() {
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(4, 2));
+
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new GridLayout(1, 2));
+        p.add(new JLabel("Status:"));
+        ButtonGroup group = new ButtonGroup();
+
+        JRadioButton enabledButton = new JRadioButton("enabled");
+        enabledButton.setActionCommand("enabled");
+        enabledButton.setSelected(true);
+        group.add(enabledButton);
+        radioPanel.add(enabledButton);
+
+        JRadioButton disabledButton = new JRadioButton("disabled");
+        disabledButton.setActionCommand("disabled");
+        disabledButton.setSelected(false);
+        group.add(disabledButton);
+        radioPanel.add(disabledButton);
+
+        p.add(radioPanel);
+
+        p.add(new JLabel("Temperature: "));
+        currentTemperatureLabel = new JLabel("0");
+        p.add(currentTemperatureLabel);
+
+
+        p.add(new JLabel("Minimum temperature:"));
+        minTemperatureField = new JTextField(15);
+        p.add(minTemperatureField);
+
+        p.add(new JLabel("Maximum temperature:"));
+        maxTemperatureField = new JTextField(15);
+        p.add(maxTemperatureField);
+
+        getContentPane().add(p, BorderLayout.CENTER);
+    }
+
+    private void createSaveButton(){
+        JButton addButton = new JButton("Save");
+        //addButton.addActionListener(new SetPreferredTemperatureListener());
+        JPanel p = new JPanel();
+        p.add(addButton);
+        getContentPane().add(p, BorderLayout.SOUTH);
     }
 
     public void updateControllersBox(AID[] controllers) {
@@ -47,5 +100,9 @@ public class SupervisorGui extends JFrame {
             box.addItem(controller.getName());
         }
     }
+
+
+
+
 
 }
